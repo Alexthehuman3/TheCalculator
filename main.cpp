7b066044e1fa1b5d3e4744d7b7f92ab105d8eae9
Alex Wong <wai14.wong@live.uwe.ac.uk>
2019-10-06 20:39:59 +0100

    User Experience Improvement - Quit confirmation.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,68 @@
 #include <iostream>
+#include <cctype>
+#include <cstring>
+
+
+inline void WaitEnter()
+{
+    std::cout << "Press Enter to continue...";
+    while (std::cin.get()!='\n');
+}
+
+bool areYouSure()
+{
+    bool confirmation = true;
+    bool return_Statement;
+    std::string yesno;
+
+    while (confirmation)
+    {
+        std::cout << "Are you sure? (y/n):";
+        std::cin >> yesno;
+
+        if (yesno == "y")
+        {
+            return_Statement = true;
+            confirmation = false;
+        }
+        else if (yesno == "n")
+        {
+            return_Statement = false;
+            confirmation = false;
+        }
+        else
+        {
+            std::cout << "Invalid Input, please try again!\n";
+        }
+    }
+
+    /*
+    // ============= Attempt 2 =================
+    bool return_Statement;
+    switch (yesno)
+    {
+        case 1:
+            return_Statement = true;
+            break;
+        case 2:
+            return_Statement = false;
+            break;
+        default:
+            std::cout << "Invalid Number, please try again!\n";
+            return_Statement = false;
+            break;
+    }
+     */
+    return return_Statement;
+}
 
 float addition()
 {
     int NUMBER_OF_NUMS;
     float USER_INPUT;
     int TOTAL_ADDITION = 0;
-    std::cout << "You have chosen Addition\n";
-    std::cout << "How many numbers would you like to add? :";
+    std::cout << "You have chosen Addition\n"
+                 "How many numbers would you like to add? :";
     std::cin >> NUMBER_OF_NUMS;
     for (int i = 1; i < NUMBER_OF_NUMS+1; i++)
     {
@@ -21,6 +77,7 @@ float addition()
 float subtraction()
 {
     int AMOUNT_TO_SUBTRACT = 0;
+    return 0;
 }
 
 void menu()
@@ -33,14 +90,13 @@ void menu()
         int CHOICE;
         int SUM_OF_FUNCTION = 0;
         std::cout << "IMPORTANT: Please type a number then press Enter to access the functions provided in the menu\n";
-        std::cout << "Type anything and press enter to continue: ";
-        std::cin >> enter_To_Continue;
+        WaitEnter();
         std::cout << "========== Main Menu ==========\n"
                      "1.) Addition\n"
                      "2.) Subtraction (WIP)\n"
-                     "0.) Quit\n";
-        std::cout << "===============================\n";
-        std::cout << "Choice? :";
+                     "0.) Quit\n"
+                     "===============================\n"
+                     "Choice? :";
         std::cin >> CHOICE;
         if (CHOICE == 1)
         {
@@ -50,7 +106,11 @@ void menu()
 
         if (CHOICE == 0)
         {
-            menu_Active = false;
+            bool tempBool = areYouSure();
+            if (tempBool)
+            {
+                menu_Active = false;
+            }
         }
     }
 }
