3b68235e7c49cab040452dc027ab8bde62e1a494
Alex Wong <wai14.wong@live.uwe.ac.uk>
2019-10-06 18:01:12 +0100

    Menu, Aesthetics, and Functions

--- main.cpp
+++ main.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Welcome to my program" << std::endl;
-    int X = 0;
-    int Y = 3;
-    std::cout << "Insert number x here: ";
-    std::cin >> X;
-    std::cout << "Insert number y here: ";
-    std::cin >> Y;
-    std::cout <<"Your added number is "<< X+Y << std:: endl;
-    return 0;
-}
-
-float addition(float x, float y)
+float addition()
 {
     int NUMBER_OF_NUMS;
+    float USER_INPUT;
+    int TOTAL_ADDITION = 0;
     std::cout << "You have chosen Addition\n";
     std::cout << "How many numbers would you like to add? :";
     std::cin >> NUMBER_OF_NUMS;
-    std::cout << "Number 1: ";
-    std::cin >> x;
+    for (int i = 1; i < NUMBER_OF_NUMS+1; i++)
+    {
+        std::cout << "Index " << i << std::endl;
+        std::cout << "Number " << i << ":";
+        std::cin >> USER_INPUT;
+        TOTAL_ADDITION += USER_INPUT;
+    }
+    return TOTAL_ADDITION;
 }
+
+float subtraction()
+{
+    int AMOUNT_TO_SUBTRACT = 0;
+}
+
+void menu()
+{
+    bool menu_Active = true;
+
+    while (menu_Active)
+    {
+        std::string enter_To_Continue;
+        int CHOICE;
+        int SUM_OF_FUNCTION = 0;
+        std::cout << "IMPORTANT: Please type a number then press Enter to access the functions provided in the menu\n";
+        std::cout << "Type anything and press enter to continue: ";
+        std::cin >> enter_To_Continue;
+        std::cout << "========== Main Menu ==========\n"
+                     "1.) Addition\n"
+                     "2.) Subtraction (WIP)\n"
+                     "0.) Quit\n";
+        std::cout << "===============================\n";
+        std::cout << "Choice? :";
+        std::cin >> CHOICE;
+        if (CHOICE == 1)
+        {
+            SUM_OF_FUNCTION = addition();
+            std::cout << "The answer is "<< SUM_OF_FUNCTION << std::endl;;
+        }
+
+        if (CHOICE == 0)
+        {
+            menu_Active = false;
+        }
+    }
+}
+
+
+
+int main() {
+    menu();
+}
+
+
+
